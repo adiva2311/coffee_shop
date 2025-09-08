61f535fecea2e5c29db847dfad36376f2ac9db7a
Adiva Nursuandy Ritonga <adiva.ritonga23@gmail.com>
2025-09-08 22:32:35 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	api_host := os.Getenv("API_HOST")
+	api_port := os.Getenv("API_PORT")
+	e.Logger.Fatal(e.Start(api_host + ":" + api_port))
+}
